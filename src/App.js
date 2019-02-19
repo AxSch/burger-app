115487f857701faff908062ee2765b9c1b0f24db
K.A. Schuneman <alex_schuneman@outlook.com>
2019-02-19 23:49:03 +0000

    feat: Added the Layout functional component to the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
-import layout from './components/Layout/Layout.tsx'
+import Layout from './components/Layout/Layout.tsx'
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.app}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={styles['App-link']}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Layout>
+          <h1>Hey there!</h1>
+        </Layout>
       </div>
     );
   }
