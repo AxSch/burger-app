5762197ddcb6557bc7f020275af106637cadcdc4
K.A. Schuneman <alex_schuneman@outlook.com>
2019-02-20 21:48:31 +0000

    style: Imported component's style and applied

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Layout.module.scss'
 
 const layout = (props: any) => (
   <>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <div>
+    <div className={styles['layout-inner']}>
       {props.children}
     </div>
   </>
