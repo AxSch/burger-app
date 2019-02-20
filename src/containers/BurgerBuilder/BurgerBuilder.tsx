50fa7ad4339c598562af12ea8357bd699b5dedbd
K.A. Schuneman <alex_schuneman@outlook.com>
2019-02-20 00:00:58 +0000

    feat: Added basic component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface IBurgerBuilderState {
+  
+}
+
+const burgerBuilder = () => {
+  return (
+    <>
+      <div>Burger</div>
+      <div>Controls</div>
+    </>
+  )
+}
+
+export default burgerBuilder
