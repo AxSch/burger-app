93708efb50626a172ebfc8e9b593779661c2168e
K.A. Schuneman <alex_schuneman@outlook.com>
2019-02-24 14:42:53 +0000

    feat: Added the Burger component to be rendered & added state management

--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import Burger from '../../components/Burger/Burger'
+import styled from 'styled-components'
 
+export interface IBurgerIngredients {
+  salad: number,
+  bacon: number,
+  cheese: number,
+  meat: number,
+}
 interface IBurgerBuilderState {
-  
+  ingredients: IBurgerIngredients
 }
 
-const burgerBuilder = () => {
+const initialState = {
+    salad: 2,
+    bacon: 2,
+    cheese: 2,
+    meat: 1,
+}
+
+const burgerBuilder: React.FunctionComponent<{}>= () => {
+  const [ingrednts, setIngredients] = React.useState(initialState)
   return (
     <>
-      <div>Burger</div>
-      <div>Controls</div>
+      <Burger ingredients={ingrednts} />
+      <div>Burger controls</div>
     </>
   )
 }
