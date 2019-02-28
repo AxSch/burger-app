d767ba9c7c91b7d964c8bbb7009566023af07636
AxSch <alex_schuneman@outlook.com>
2019-02-28 21:37:39 +0000

    style: Added a More & Less style class along with selectors

--- src/components/Burger/BuildControls/BuildControl/BuildControl.tsx
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.tsx
@@ -9,6 +9,28 @@ const StyledControl = styled.div`
   box-shadow: 0 2px 1px #ccc;
   margin: 5px 0;
   padding: 10px 0;
+
+  .Less {  
+    background-color: #D39952;
+    color: white;
+  }
+
+  .More {
+    background-color: #8F5E1E;
+    color: white;
+  }
+
+  .Less:hover, .Less:active {  
+    background-color: #DAA972;
+    color: white;
+  }
+
+  .More:hover, .More:active {
+    background-color: #99703F;
+    color: white;
+  }
+  
+  
 `
 
 const StyledLabel = styled.div`
@@ -27,18 +49,18 @@ const StyledButton = styled.button`
   cursor: pointer;
   outline: none;
 
-:disabled {
-  background-color: #AC9980;
-  border: 1px solid #7E7365;
-  color: #ccc;
-  cursor: default;
-}
+  :disabled {
+    background-color: #AC9980;
+    border: 1px solid #7E7365;
+    color: #ccc;
+    cursor: default;
+  }
 
-:hover:disabled {
-  background-color: #AC9980;
-  color: #ccc;
-  cursor: not-allowed;
-}
+  :hover:disabled {
+    background-color: #AC9980;
+    color: #ccc;
+    cursor: not-allowed;
+  }
 `
 
 interface IbuildControlProps {
@@ -52,8 +74,8 @@ const buildControl: React.FunctionComponent<IbuildControlProps> = ({ingredientLa
   return (
     <StyledControl>
       <StyledLabel>{ingredientLabel}</StyledLabel>
-      <StyledButton onClick={() => addRemHandlers.addHandler(type)}>Add</StyledButton>
-      <StyledButton disabled={isDisabled[type]} onClick={() => addRemHandlers.subHandler(type)}>Remove</StyledButton>
+      <StyledButton className="More" onClick={() => addRemHandlers.addHandler(type)}>Add</StyledButton>
+      <StyledButton className="Less" disabled={isDisabled[type]} onClick={() => addRemHandlers.subHandler(type)}>Remove</StyledButton>
     </StyledControl>
   )
 }
