415a3f43f6c95e39ab5c51b2a8d364e6c7e6f89e
AxSch <alex_schuneman@outlook.com>
2019-02-28 00:08:00 +0000

    feat: Added renderModal() & isVisible prop
    msg: renderModal() uses isVisible to decide whether to render or not

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+interface IModal {
+  isVisible: boolean
+}
 
-class Modal extends React.Component {
+class Modal extends React.Component<IModal> {
   constructor(props){
     super(props)
     this.modal = document.createElement('div')
@@ -17,10 +20,22 @@ class Modal extends React.Component {
     document.body.removeChild(this.modal)
   }
 
+  private renderModal = isVisible => {
+    if (isVisible) {
+      return ReactDOM.createPortal(
+        this.props.children,
+        this.modal
+      )
+    }
+    return null
+  }
+
   public render() {
-    return ReactDOM.createPortal(
-      this.props.children,
-      this.modal
+    const { isVisible } = this.props
+    return (
+      <>
+        {this.renderModal(isVisible)}
+      </>
     )
   }
 }
