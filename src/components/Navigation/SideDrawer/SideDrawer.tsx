fc4fab4c7dca5e58084e90a0dd81f51c2bda8af7
AxSch <alex_schuneman@outlook.com>
2019-03-04 15:09:00 +0000

    feat: Added basic SideDrawer component

---
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledSideDrawer = styled.div`
+
+`
+
+interface ISideDrawer {
+
+}
+
+const SideDrawer: React.FunctionComponent<ISideDrawer> = () => {
+  return (
+    <StyledSideDrawer>
+    </StyledSideDrawer>
+  )
+}
+
+export default SideDrawer
