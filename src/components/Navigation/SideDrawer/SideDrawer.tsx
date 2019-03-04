dedc05ed2e18aa32351c70adba7fd21f989ce926
AxSch <alex_schuneman@outlook.com>
2019-03-04 15:47:34 +0000

    style&feat: Added NavItems to the component and also basic styling

--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+import NavItems from '../NavItems/NavItems'
 
 const StyledSideDrawer = styled.div`
+  position: fixed;
+  width: 280px;
+  max-width: 70%;
+  height: 100%
+  top: 0 
+  left: 0;
+  z-index: 200;
+  background-color: white;
+  padding: 32px 16px;
+  box-sizing: border-box;
+  transition: transform 0.3s ease-out;
 
+  @media(min-width: 500px) {
+    display: none;
+  }
+
+  &.open {
+    transform: translateX(0);
+  }
+
+  &.close {
+    transform: translateX(-100%);
+  }
 `
 
 interface ISideDrawer {
@@ -12,6 +35,9 @@ interface ISideDrawer {
 const SideDrawer: React.FunctionComponent<ISideDrawer> = () => {
   return (
     <StyledSideDrawer>
+      <nav>
+        <NavItems />
+      </nav>
     </StyledSideDrawer>
   )
 }
