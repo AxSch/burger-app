7f5c30ffc1bbfb6655dfc090e877949abbf269ad
AxSch <alex_schuneman@outlook.com>
2019-03-04 12:30:32 +0000

    feat: Added totalPrice to component through use of context

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { IBurgerIngredients } from 'containers/BurgerBuilder/BurgerBuilder'
 import Button from '../../../components/UI/Button/Button';
+import OrderContext from '../../../context/OrderContext';
 
 interface IOrderSummary {
   ingredients: IBurgerIngredients
@@ -8,6 +9,7 @@ interface IOrderSummary {
 }
 
 const OrderSummary: FunctionComponent<IOrderSummary> = ({ ingredients, onClick }) => {
+  const context = React.useContext(OrderContext)
   const ingredientsSummary = ingredients => {
     return Object.keys(ingredients).map(ingrnt => {
       return <li key={ingrnt}><span style={{ textTransform: 'capitalize' }}>{ingrnt}</span>: {ingredients[ingrnt]}</li>
@@ -21,6 +23,8 @@ const OrderSummary: FunctionComponent<IOrderSummary> = ({ ingredients, onClick }
       <ul>
         {ingredientsSummary(ingredients)}
       </ul>
+      <p>Total Price:</p>
+      <p>£{context.totalPrice.toFixed(2)}</p>
       <p>Continue to Checkout?</p>
       <Button type="Success" onClick={onClick}>CHECKOUT</Button>
       <Button type="Danger" onClick={onClick}>CANCEL</Button>
