b1fd1ac4962a3862b01f22a1a2518ffe24ad8836
AxSch <alex_schuneman@outlook.com>
2019-03-04 16:49:22 +0000

    style: Added styling to create hamburger icon stack

--- src/components/Navigation/MenuToggle/MenuToggle.tsx
+++ src/components/Navigation/MenuToggle/MenuToggle.tsx
@@ -2,17 +2,37 @@ import React, { FunctionComponent } from 'react'
 import SideBarContext from '../../../context/SideBarContext'
 import styled from 'styled-components'
 
-const StyledToggle = styled.h1`
+const StyledToggle = styled.div`
+  width: 40px;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 0;
+  box-sizing: border-box;
+  cursor: pointer;
+  
   @media(min-width: 500px) {
     display: none
   }
+`
+
+const StyledToggleIcon = styled.div`
+  width: 90%;
+  height: 3px;
+  background-color: white;
 `
 
 const MenuToggle:FunctionComponent = () => {
   const context = React.useContext(SideBarContext)
   return (
     <>
-      <StyledToggle onClick={() => context.setIsVisible(!context.isVisible)}>Menu</StyledToggle>
+      <StyledToggle onClick={() => context.setIsVisible(!context.isVisible)}>
+        <StyledToggleIcon></StyledToggleIcon>
+        <StyledToggleIcon></StyledToggleIcon>
+        <StyledToggleIcon></StyledToggleIcon>
+      </StyledToggle>
     </>
   )
 }
