00e08e60c0e491cb6ca3b0ebae16e0b30002f951
AxSch <alex_schuneman@outlook.com>
2019-03-04 12:23:34 +0000

    feat: Added prop onclick and passed to Button component component

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import { IBurgerIngredients } from 'containers/BurgerBuilder/BurgerBuilder'
+import Button from '../../../components/UI/Button/Button';
 
 interface IOrderSummary {
   ingredients: IBurgerIngredients
+  onClick: Function
 }
 
-const OrderSummary: FunctionComponent<IOrderSummary> = ({ ingredients }) => {
+const OrderSummary: FunctionComponent<IOrderSummary> = ({ ingredients, onClick }) => {
   const ingredientsSummary = ingredients => {
     return Object.keys(ingredients).map(ingrnt => {
       return <li key={ingrnt}><span style={{ textTransform: 'capitalize' }}>{ingrnt}</span>: {ingredients[ingrnt]}</li>
@@ -19,6 +21,9 @@ const OrderSummary: FunctionComponent<IOrderSummary> = ({ ingredients }) => {
       <ul>
         {ingredientsSummary(ingredients)}
       </ul>
+      <p>Continue to Checkout?</p>
+      <Button type="Success" onClick={onClick}>CHECKOUT</Button>
+      <Button type="Danger" onClick={onClick}>CANCEL</Button>
     </>
   )
 }
