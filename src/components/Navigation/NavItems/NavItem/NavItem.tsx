9d9db6497c3fc61590e0150f66359914f15b77f4
AxSch <alex_schuneman@outlook.com>
2019-03-04 15:48:49 +0000

    style: Changed base styles to be mobile first

--- src/components/Navigation/NavItems/NavItem/NavItem.tsx
+++ src/components/Navigation/NavItems/NavItem/NavItem.tsx
@@ -2,29 +2,46 @@ import React from 'react'
 import  styled from 'styled-components'
 
 const StyledNavItem = styled.li`
-  margin: 0;
+  margin: 10px 0;
   box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  height: 100%;
-  width: auto;
+  display: block;
+  width: 100%;
+
+  @media(min-width: 500px) {
+    margin: 0;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: auto;
+  }
 `
 
 const StyledItemLink = styled.a`
-  color: white;
+  color: #8F5C2C;
   text-decoration: none;
-  height: 100%;
-  padding: 16px 10px;
-  border-bottom: 4px solid transparent;
+  width: 100%;
   box-sizing: border-box;
   display: block;
 
   :hover,
   :active,
   &.active {
-    background-color: #8F5C2C;
-    border-bottom: 4px solid #40A4C8;
+    color: #40A4C8;
+  }
+
+  @media(min-width: 500px) {
     color: white;
+    height: 100%;
+    padding: 16px 10px;
+    border-bottom: 4px solid transparent;
+
+    :hover,
+    :active,
+    &.active {
+      background-color: #8F5C2C;
+      border-bottom: 4px solid #40A4C8;
+      color: white;
+    }
   }
 `
 
