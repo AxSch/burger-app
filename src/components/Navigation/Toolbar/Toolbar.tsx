5036030b65d8fa4af3d1fb42ea712671e9984407
AxSch <alex_schuneman@outlook.com>
2019-03-04 12:44:09 +0000

    feat: Created a basic toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledToolBar = styled.header`
+  height: 56px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-colour: #703B09;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+  z-index: 90;
+`
+
+const StyledToolBarNav = styled.nav`
+  height: 100%;
+`
+
+const ToolBar: React.FunctionComponent<{}> = () => {
+  return (
+    <StyledToolBar>
+      <div>LOGO</div>
+      <div>MENU</div>
+      <StyledToolBarNav>
+        ...
+      </StyledToolBarNav>
+    </StyledToolBar>
+  )
+}
+
+export default ToolBar
