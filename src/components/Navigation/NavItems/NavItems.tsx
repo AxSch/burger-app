521a9dc957bb9d64f8f173df0631f8aefda29b1b
AxSch <alex_schuneman@outlook.com>
2019-03-14 23:13:59 +0000

    refact: Altered the link paths

--- src/components/Navigation/NavItems/NavItems.tsx
+++ src/components/Navigation/NavItems/NavItems.tsx
@@ -19,8 +19,8 @@ const StyledNavItems = styled.ul`
 const NavItems: React.FunctionComponent = () => {
   return (
     <StyledNavItems>
-      <NavItem isActive link="hello">Home</NavItem>
-      <NavItem isActive={false} link="hello">Checkout</NavItem>
+      <NavItem isActive link="/">Home</NavItem>
+      <NavItem isActive={false} link="/checkout">Checkout</NavItem>
     </StyledNavItems>
   )
 }
