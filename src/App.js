ad8de04cbd20cfac216ea36117c7a23e426678a4
AxSch <alex_schuneman@outlook.com>
2019-03-14 23:10:08 +0000

    feat: Made use of router Switch & Route components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout.tsx'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.tsx'
 import Checkout from './containers/Checkout/Checkout.tsx'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
