fce60a60a5d1edc3292ad6c14e9982c66e392ac0
AxSch <alex_schuneman@outlook.com>
2019-03-14 23:37:15 +0000

    feat: Added cancelCheckout() & continueCheckout()

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import CheckoutSummary from '../../components/Checkout/CheckoutSummary/CheckoutSummary'
 import { IBurgerIngredients } from '../../containers/BurgerBuilder/BurgerBuilder'
+import { RouteComponentProps } from 'react-router-dom'
 
 
 interface ICheckoutState {
   ingredients: IBurgerIngredients
 }
 
-class Checkout extends React.Component<{},ICheckoutState> {
+class Checkout extends React.Component<{} & RouteComponentProps,ICheckoutState> {
   constructor(props){
     super(props)
     this.state = {
@@ -16,14 +17,28 @@ class Checkout extends React.Component<{},ICheckoutState> {
         cheese: 0,
         meat: 0,
         bacon: 0,
-      }
+      },
     }
   }
+  private cancelCheckout = () => {
+    const { history } = this.props
+    history.goBack()
+  }
+  
+  private continueCheckout = () => {
+    const { history } = this.props
+    history.push("/checkout")
+  }
+
   public render() {
     const { ingredients } = this.state
     return (
       <div>
-        <CheckoutSummary ingredients={ingredients} onClick={()=>{}} />
+        <CheckoutSummary 
+          ingredients={ingredients} 
+          continueCheckout={this.continueCheckout}
+          cancelCheckout={this.cancelCheckout}
+        />
       </div>
     )
   }
