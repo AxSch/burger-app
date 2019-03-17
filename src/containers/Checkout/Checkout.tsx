a07f48224c4f024c3120787ec70cb36ee7dde36c
AxSch <alex_schuneman@outlook.com>
2019-03-17 21:59:06 +0000

    feat: Added route to render basic contactData container & changed the push path

--- src/containers/Checkout/Checkout.tsx
+++ src/containers/Checkout/Checkout.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import CheckoutSummary from '../../components/Checkout/CheckoutSummary/CheckoutSummary'
 import { IBurgerIngredients } from '../../containers/BurgerBuilder/BurgerBuilder'
-import { RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps, Route } from 'react-router-dom'
 import * as queryString from 'query-string'
+import ContactData from '../../components/Checkout/ContactData/ContactData';
 
 
 interface ICheckoutState {
   ingredients: IBurgerIngredients
 }
 
-class Checkout extends React.Component<{} & RouteComponentProps,ICheckoutState> {
-  constructor(props){
+class Checkout extends React.Component<{} & RouteComponentProps, ICheckoutState> {
+  constructor(props) {
     super(props)
     this.state = {
       ingredients: {
@@ -25,7 +26,7 @@ class Checkout extends React.Component<{} & RouteComponentProps,ICheckoutState> 
   componentDidMount() {
     const { location } = this.props
     let ingrdObj = {} as IBurgerIngredients
-    
+
     const query = location.search
     const params = queryString.parse(query)
     for (const ingrd in params) {
@@ -43,24 +44,27 @@ class Checkout extends React.Component<{} & RouteComponentProps,ICheckoutState> 
     const { history } = this.props
     history.goBack()
   }
-  
+
   private continueCheckout = () => {
-    const { history } = this.props
-    history.push("/checkout")
+    const { history, match } = this.props
+    history.push(match.url + "/contact-data")
   }
 
   public render() {
     const { ingredients } = this.state
+    const { match } = this.props
+    console.log(match.url)
     return (
       <div>
-        <CheckoutSummary 
-          ingredients={ingredients} 
+        <CheckoutSummary
+          ingredients={ingredients}
           continueCheckout={this.continueCheckout}
           cancelCheckout={this.cancelCheckout}
         />
+        <Route path={match.url + '/contact-data'} component={ContactData} />
       </div>
-    )
+      )
+    }
   }
-}
-
-export default Checkout
+  
+  export default Checkout
