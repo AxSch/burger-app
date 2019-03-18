ed18b44491f201758db1bd35bdc5bcc33ce7e0aa
AxSch <alex_schuneman@outlook.com>
2019-03-18 21:12:53 +0000

    feat: Added a route for the Orders container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout.tsx'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.tsx'
 import Checkout from './containers/Checkout/Checkout.tsx'
+import Orders from './containers/Orders/Orders.tsx'
 import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout}/>
+            <Route path="/orders" exact component={Orders}/>
             <Route path="/" exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
